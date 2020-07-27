9d28f7346d8c2ea3be1c05739e7fb1a7b26a1745
Leigh Passamano <leighayna.josele@gmail.com>
2020-07-27 09:27:13 -0400

    Bot iterates over all options for a guess then records the points. After finding the point value of every guess, the bot then repeatedly selects the option with the highest point value.

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ class MyBot extends GuessBot {
     super()
     this.banditBotId = BANDIT_BOT_ID
     this.start()
+    this.actions = {}
+    this.maxValue = 100
   }
+
+  updateActions(action, value) {
+    this.actions[action] = value
+  }
+  // store guesses and points
 
   handleGameStart(actionSpace, banditBotId) {
     if (banditBotId) {
       this.banditBotId = `<@${banditBotId}>`
     }
     this.space = actionSpace
+    actionSpace.map(s => this.updateActions(s, null))
     this.send(`Wow this is hard, what should I choose? ${this.space.join(', ')}`)
-    this.makeGuess(this.space[0])
+    this.makeGuess(this.getGuess())
   }
 
   handleReward ({reward, guess, totalScore, remaining}, banditBotId) {
@@ -23,14 +31,24 @@ class MyBot extends GuessBot {
 
     // This is where your guess logic goes. The idea is to maximize your total points.
     // Currently, I'm just iterating over the whole action space.
-    if (remaining > 0) {
-      const nextGuessIndex = (this.space.indexOf(guess) + 1) % this.space.length
-      this.makeGuess(this.space[nextGuessIndex])
+    if (remaining > 0 && reward === this.maxValue) {
+      this.makeGuess(guess)
     }
   }
 
   handleGameOver ({ totalScore }, banditBotId) {
     this.send(`I scored a total of ${totalScore} points.`)
+  }
+
+  getGuess() {
+    var guesses = Object.keys(this.actions).filter(k => this.actions[k] === null)
+    if (guesses.length > 0) {
+      return guesses[0]
+    } else {
+      const guess = Math.max.apply(null,Object.keys(this.actions).map(k => this.actions[k] ));
+      console.log(guess)
+      return guess
+    }
   }
 
   makeGuess (guess) {
